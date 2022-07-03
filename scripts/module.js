fbd684e66769beb0cc807e0f1ad77a45e43871b4
danielrab.work <danielrab.work@gmail.com>
2022-07-03 21:40:38 +0300

    fix all combatants being renmaed to New Combatant

--- scripts/module.js
+++ scripts/module.js
@@ -2,7 +2,7 @@ Hooks.once('init', async () => {
   const createDocuments = foundry.abstract.Document.createDocuments;
   foundry.abstract.Document.createDocuments = function newCreateDocuments(dataArray=[], ...args) {
     dataArray.forEach(data => {
-      if (!data.name) data.name = `New ${this.documentName}`;
+      if (!data.name && this.documentName !== 'Combatant') data.name = `New ${this.documentName}`;
     })
     return createDocuments.call(this, dataArray, ...args);
   };
